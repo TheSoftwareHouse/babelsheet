23e20f745a3e43bdf872d5b8b96ccc1fdc58df7c
Paweł Skotnicki <pawel.skotnicki@thesoftwarehouse.pl>
2017-07-03 08:21:32 +0200

    Use Symfony\Console component for converting xlf->spreadsheet

---
+++ application.php
@@ -0,0 +1,38 @@
+#!/usr/bin/env php
+<?php
+
+set_time_limit(0);
+
+require __DIR__.'/vendor/autoload.php';
+
+use GDriveTranslations\Config\Config;
+use GDriveTranslations\GDrive;
+use GDriveTranslations\GDriveDownloader;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Input\ArgvInput;
+use Symfony\Component\Console\Input\InputOption;
+
+const DEFAULT_WORK_DIR = '/lang';
+
+$input = new ArgvInput();
+$workDir = $input->getParameterOption(array('--dir', '-d'), DEFAULT_WORK_DIR);
+
+$credentialsFile = $workDir . '/translate_token.json';
+
+$GDriveServiceFactory = new GDrive($credentialsFile);
+$downloader = new GDriveDownloader($GDriveServiceFactory->getService(Config::ACCESS_DRIVE));
+
+$application = new Application('Babelsheet');
+$application->getDefinition()->addOption(new InputOption(
+    '--dir',
+    '-d',
+    InputOption::VALUE_REQUIRED,
+    'work directory',
+    DEFAULT_WORK_DIR
+));
+
+$application
+    ->add(new \GDriveTranslations\Command\ConvertCommand($downloader))
+;
+
+$application->run();
